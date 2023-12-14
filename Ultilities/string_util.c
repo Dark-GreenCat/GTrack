476f954d12e0e33b12934010874f882f82def43f
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2023-12-14 14:28:53 +0700

    [MODIDY] Modify string_util.c so that return value cannot be NULL

--- Ultilities/string_util.c
+++ Ultilities/string_util.c
@@ -1,7 +1,27 @@
 #include "string_util.h"
+#include <stdint.h>
+#include <stdio.h>
+
+char* UTIL_STRING_Duplicate(const char* String, char* CopiedString) {
+    char* p_CopiedString = CopiedString;
+    while (*String != '\0') *CopiedString++ = *String++;
+    *CopiedString = '\0';
+
+    return p_CopiedString;
+}
+
+char* UTIL_STRING_Copy(const char* String, uint32_t StartIndex, uint32_t CopiedLength, char* CopiedString){
+    char* p_CopiedString = CopiedString;
+    while (CopiedLength--) {
+        *CopiedString++ = String[StartIndex++];
+    }
+    *CopiedString = '\0';
+
+    return p_CopiedString;
+}
 
 int16_t UTIL_STRING_compareDifference(const char* String, const char* CompareString) {
-    int16_t Difference;
+    int16_t Difference = 0;
     while (*String != '\0' && *CompareString != '\0') {
          Difference = *String++ - *CompareString++;
          if(Difference != 0) break;
@@ -13,8 +33,8 @@ int16_t UTIL_STRING_compareDifference(const char* String, const char* CompareStr
 }
 
 int16_t UTIL_STRING_compareDifferenceFirstNChar(const char* String, const char* CompareString, const uint32_t NumberOfChar) {
-    int16_t Difference;
-    uint32_t Index;
+    int16_t Difference = 0;
+    uint32_t Index = 0;
     for(Index = 0; Index < NumberOfChar; Index++) {
         if(String[Index] == '\0' || CompareString[Index] == '\0') break;
         Difference = String[Index] - CompareString[Index];
@@ -36,22 +56,18 @@ bool UTIL_STRING_comparedTo(const char* String, const char* CompareString) {
 }
 
 uint32_t UTIL_STRING_getLength(const char* String) {
-    uint32_t count = 0;
-    while (*String++ != '\0') {
-        count++;
-    }
+    uint32_t Count = 0;
+    while (*String++ != '\0') Count++;
 
-    return count;
+    return Count;
 }
 
 int32_t UTIL_STRING_indexOf(const char* String, const char Character) {
-    int32_t index = -1;
+    int32_t Index = -1;
 
     while (*String != '\0') {
-        index++;
-        if (*String == Character) {
-            return index;
-        }
+        Index++;
+        if (*String == Character) return Index;
         String++;
     }
 
@@ -59,42 +75,33 @@ int32_t UTIL_STRING_indexOf(const char* String, const char Character) {
 }
 
 char* UTIL_STRING_getSubStringFromIndex(const char* String, uint32_t Index, char* Destination) {
-    char* Destination_Begin = Destination;
-
-    const char* SubString = String + Index; 
-    while (*SubString != '\0') {
-        *Destination++ = *SubString++;
-    }
-    *Destination = '\0';
-
-    return Destination_Begin;
+    return UTIL_STRING_Duplicate(String + Index, Destination);
 }
 
 char* UTIL_STRING_getSubStringBetweenIndex(const char* String, uint32_t Index_Begin, uint32_t Index_End, char* Destination) {
-    char* Destination_Begin = Destination;
-
-    for(uint32_t Index = Index_Begin; Index <= Index_End; Index++) {
-        *Destination++ = String[Index];
-    }
-    *Destination = '\0';
-
-    return Destination_Begin;
+    return UTIL_STRING_Copy(String, Index_Begin, Index_End - Index_Begin + 1, Destination);
 }
 
 char* UTIL_STRING_getSubStringFromChar(const char* String, const char Character, char* Destination) {
-    uint32_t Index = UTIL_STRING_indexOf(String, Character);
+    uint32_t Index = (uint32_t) UTIL_STRING_indexOf(String, Character);
+    if(Index == (uint32_t) -1) {
+        *Destination = '\0';
+        return Destination;
+    }
     return UTIL_STRING_getSubStringFromIndex(String, Index, Destination);
 }
 
-char* UTIL_STRING_getSubStringTerminatedChar(const char* String, uint32_t Char_Begin, uint32_t Char_Terminated, char* Destination) {
-    char* Destination_Begin = Destination;
-    uint32_t Index_Begin = UTIL_STRING_indexOf(String, Char_Begin);
-
-    const char* SubString = String + Index_Begin;
-    while (*SubString != Char_Terminated && *SubString != '\0') {
-        *Destination++ = *SubString++;
+char* UTIL_STRING_getSubStringBeforeChar(const char* String, char TerminatedChar, char* Destination) {
+    uint32_t Index_End = 0;
+    while (String[Index_End] != TerminatedChar && String[Index_End] != '\0') {  
+        Index_End++;
     }
-    *Destination = '\0';
 
-    return Destination_Begin;
+    return UTIL_STRING_Copy(String, 0, Index_End, Destination);
+}
+
+char* UTIL_STRING_getSubStringAfterChar(const char* String, char AfterChar, char* Destination) {
+    uint32_t Index = (uint32_t) UTIL_STRING_indexOf(String, AfterChar);
+
+    return UTIL_STRING_getSubStringFromIndex(String, Index + 1, Destination);
 }
--- Ultilities/string_util.h
+++ Ultilities/string_util.h
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+char* UTIL_STRING_Duplicate(const char* String, char* CopiedString);
+char* UTIL_STRING_Copy(const char* String, uint32_t StartIndex, uint32_t CopiedLength, char* CopiedString);
 int16_t UTIL_STRING_compareDifference(const char* String, const char* CompareString);
 int16_t UTIL_STRING_compareDifferenceFirstNChar(const char* String, const char* CompareString, const uint32_t NumberOfChar);
 bool UTIL_STRING_isStartWith(const char* String, const char* StartString);
@@ -14,6 +16,7 @@ int32_t UTIL_STRING_indexOf(const char* String, const char Character);
 char* UTIL_STRING_getSubStringFromIndex(const char* String, uint32_t Index, char* Destination);
 char* UTIL_STRING_getSubStringBetweenIndex(const char* String, uint32_t Index_Begin, uint32_t Index_End, char* Destination);
 char* UTIL_STRING_getSubStringFromChar(const char* String, const char Character, char* Destination);
-char* UTIL_STRING_getSubStringTerminatedChar(const char* String, uint32_t Char_Begin, uint32_t Char_Terminated, char* Destination);
+char* UTIL_STRING_getSubStringBeforeChar(const char* String, char TerminatedChar, char* Destination);
+char* UTIL_STRING_getSubStringAfterChar(const char* String, char AfterChar, char* Destination);
 
 #endif
