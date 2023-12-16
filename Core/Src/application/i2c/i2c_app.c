78a84bed7616adb20f41e75070e900f55d241264
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2023-12-16 16:23:51 +0700

    [REBUILD] Change i2c_app to i2c_hcl and put to HCL/ folder

--- Core/Src/application/i2c/i2c_app.c
+++
@@ -1,5 +0,0 @@
-#include "i2c_app.h"
-
-void HCL_I2C_Mem_Read(I2C_HandleTypeDef* hi2c, uint16_t dev_addr, uint16_t reg_addr, uint8_t* p_data, uint16_t read_length) {
-    HAL_I2C_Mem_Read(hi2c, (dev_addr << 1), reg_addr, 1, p_data, read_length, HCL_I2C_TIMEOUT);
-}
