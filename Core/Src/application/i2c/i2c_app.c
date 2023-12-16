76bdd1208193e9b01d6ac34132d2b501af2b6c6a
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2023-12-16 15:43:47 +0700

    [ADD] Add I2C Read function

--- Core/Src/application/i2c/i2c_app.c
+++ Core/Src/application/i2c/i2c_app.c
@@ -1,3 +1,5 @@
 #include "i2c_app.h"
 
-I2C_HandleTypeDef* hi2c_accelemeter;
+void HCL_I2C_Mem_Read(I2C_HandleTypeDef* hi2c, uint16_t dev_addr, uint16_t reg_addr, uint8_t* p_data, uint16_t read_length) {
+    HAL_I2C_Mem_Read(hi2c, (dev_addr << 1), reg_addr, 1, p_data, read_length, HCL_I2C_TIMEOUT);
+}
