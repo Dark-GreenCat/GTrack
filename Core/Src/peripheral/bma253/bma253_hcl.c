c69206d6b416a9c28a46440a4c03ce00984e4146
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2023-12-16 21:37:53 +0700

    [ADD] Add p_bma253_current to ensure when interrupt happen the value of p_bma253 does not change

--- Core/Src/peripheral/bma253/bma253_hcl.c
+++ Core/Src/peripheral/bma253/bma253_hcl.c
@@ -6,6 +6,9 @@ void HCL_BMA253_Init(bma253_t* bma253) {
     uint8_t data_u8 = BMA2x2_INIT_VALUE;
     uint8_t config_data_u8 = BMA2x2_INIT_VALUE;
     
+    /* Backup the global value */
+    bma253_t* p_bma253_current = p_bma253;
+
     p_bma253 = bma253;
     /* Read ChipID */
     p_bma253->read(p_bma253->dev_addr, BMA2x2_CHIP_ID_REG, &data_u8, BMA2x2_GEN_READ_WRITE_LENGTH);
@@ -14,12 +17,27 @@ void HCL_BMA253_Init(bma253_t* bma253) {
     /* Read Fifo Config Register */
     p_bma253->read(p_bma253->dev_addr, BMA2x2_FIFO_MODE_REG, &config_data_u8, BMA2x2_GEN_READ_WRITE_LENGTH);
     p_bma253->fifo_config = config_data_u8;
+
+    /* Reassign the global value */
+    p_bma253 = p_bma253_current;
 }
 
 void HCL_BMA253_Read_Reg(uint8_t reg_addr, uint8_t* data_u8, uint8_t len_u8) {
+    /* Backup the global value */
+    bma253_t* p_bma253_current = p_bma253;
+
     p_bma253->read(p_bma253->dev_addr, reg_addr, data_u8, len_u8);
+
+    /* Reassign the global value */
+    p_bma253 = p_bma253_current;
 }
 
 void HCL_BMA253_Write_Reg(uint8_t reg_addr, uint8_t* data_u8, uint8_t len_u8) {
+    /* Backup the global value */
+    bma253_t* p_bma253_current = p_bma253;
+
     p_bma253->write(p_bma253->dev_addr, reg_addr, data_u8, len_u8);
+
+    /* Reassign the global value */
+    p_bma253 = p_bma253_current;
 }
